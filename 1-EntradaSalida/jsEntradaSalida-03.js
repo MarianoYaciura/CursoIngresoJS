1eb188c835823beca3f8b2b065c567a820a88373
Mariano <yaciuramariano7@gmail.com>
2022-02-03 18:17:34 -0300

    Nuevos Cambios

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,11 +1,16 @@
 /*
+Yaciura Mariano
+Ejercicio 3*/
+/*
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	let nombreIngresado;
+
+	 nombreIngresado= document.getElementById("txtIdNombre").value;
+	
+	alert(nombreIngresado);
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,17 @@
 /* 
+Yaciura Mariano
+Ejercicio 4*/
+/*
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let nombre;
+	
+	nombre= prompt("Ingresar nombre");
+	
+	document.getElementById("txtIdNombre").value= nombre;
+
 }
 
