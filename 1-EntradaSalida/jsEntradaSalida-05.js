d8ff82aaeba2f9d7f147aafbcbee4770b81c88b4
Mariano <yaciuramariano7@gmail.com>
2022-02-04 15:15:53 -0300

    Nuevos cambios

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,17 +1,19 @@
 /*
-Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+Yaciura Mariano
+Ejercicio 5*/
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
 	let nombreIngresado;
+    let numeroIngresado;
 
 	 nombreIngresado= document.getElementById("txtIdNombre").value;
-	
-	let numeroIngresado;
-
 	 numeroIngresado= document.getElementById("txtIdEdad").value;
 
-	alert(nombreIngresado+numeroIngresado);
+	 mensajeAMostrar= "Usted se llama " + nombreIngresado + " y tiene " + numeroIngresado + " edad";
+	
+	 alert(mensajeAMostrar);
 
 }
 
